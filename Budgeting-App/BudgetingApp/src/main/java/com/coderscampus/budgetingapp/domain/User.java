4bade9a9c4b0b5c39de6e4093704b06e6a2ad0c9
Hal Vong <halvong@yahoo.com>
2020-12-24 12:20:49 -0800

    Initial commit

---
+++ Budgeting-App/BudgetingApp/src/main/java/com/coderscampus/budgetingapp/domain/User.java
@@ -0,0 +1,77 @@
+package com.coderscampus.budgetingapp.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@Table(name="users")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class User
+{
+  private Long id;
+  private String username;
+  private String password;
+  private String confirmPassword;
+  private Set<Budget> budgets = new TreeSet<>();
+
+  @Id
+  @GeneratedValue
+  public Long getId()
+  {
+    return id;
+  }
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+  public String getUsername()
+  {
+    return username;
+  }
+  public void setUsername(String username)
+  {
+    this.username = username;
+  }
+  public String getPassword()
+  {
+    return password;
+  }
+  public void setPassword(String password)
+  {
+    this.password = password;
+  }
+
+  @ManyToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="users")
+  public Set<Budget> getBudgets()
+  {
+    return budgets;
+  }
+  public void setBudgets(Set<Budget> budgets)
+  {
+    this.budgets = budgets;
+  }
+
+  @Transient
+  public String getConfirmPassword()
+  {
+    return confirmPassword;
+  }
+  public void setConfirmPassword(String confirmPassword)
+  {
+    this.confirmPassword = confirmPassword;
+  }
+}
---
+++ Budgeting-App/README.md
@@ -0,0 +1,26 @@
+Budgeting-App, Udemy, Trevor Page
+Dec 30, 2020. Wed
+
+#tutorial
+Spring Security Authentication
+https://www.udemy.com/course/building-a-java-spring-boot-web-app-from-scratch/learn/lecture/22547026#overview
+
+#url 
+localhost:8080
+
+#github
+https://github.com/tp02ga/Budgeting-App.git
+
+#schema
+1. One Category to many Transaction
+   Many Transaction to one Category
+
+2. One User to many Group
+   Many User to one Group
+
+3. One Group to many Category
+   Many Category to one Group
+
+
+
+
